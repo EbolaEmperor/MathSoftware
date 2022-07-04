d6d16d3dda44e4bedfc8042a5a74f428ddc97de6
HUANG Wenchong <ebola@zju.edu.cn>
2022-07-04 21:57:24 +0800

    add slide

--- Final/src/paperpicgen1.sh
+++ Final/src/paperpicgen1.sh
@@ -2,5 +2,5 @@
 
 mkdir img
 ./juliaGen img/pic0.png 0 0 5 200 -0.5 0
-./juliaGen img/pic1.png 0 0 5 200 -0.72624686941319 0.240376999706965
+./juliaGen img/pic1.png 0 0 5 200 -0.726 0.240
 ./juliaGen img/pic2.png 0 0 5 200 -0.8 0.4
